d8c1c779a4072d452326671f7e36b3b1858d41aa
fati <etsukazu200@gmail.com>
2021-11-03 16:16:45 +0100

    installation spring

---
+++ src/principale/PrincipeClass.java
@@ -0,0 +1,22 @@
+package principale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class PrincipeClass {
+
+	
+
+		
+		
+		public static void main(String[] args) {
+			SpringApplication.run( PrincipeClass.class, args);
+		
+	
+	
+	
+		}
+}
